c7356c0e20c5d698a0a4e536288c150ee59bcf40
matanlurey <matanlurey@users.noreply.github.com>
2020-05-14 00:04:01 +0000

    Update dist to output generated at f1a6139

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -11,14 +30,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.SplitIO = exports.rewriteUrlStrings = exports.splitSave = exports.splitObject = exports.nameObject = void 0;
 const filenamify_1 = __importDefault(require("filenamify"));
 const fs = __importStar(require("fs-extra"));
 const path_1 = __importDefault(require("path"));
@@ -164,7 +177,8 @@ function rewriteUrlStrings(input, options) {
         return input;
     }
     return input.replace(matchUrls, (subString) => {
-        if (options.ban && subString.match(subString)) {
+        if (options.ban && subString.match(options.ban)) {
+            console.warn(subString.match(subString));
             throw new Error(`Unsupported URL: "${subString}" (Matched "${options.ban}")`);
         }
         if (options.from && options.to) {
@@ -373,7 +387,8 @@ class SplitIO {
     }
     readExtractedObject(file) {
         return __awaiter(this, void 0, void 0, function* () {
-            const entry = (yield this.readJson(file));
+            const rawJson = this.rewriteFromSource(yield this.readFile(file, 'utf-8'));
+            const entry = JSON.parse(rawJson);
             const states = {};
             for (const k in entry.StatesPaths) {
                 const target = path_1.default.join(path_1.default.dirname(file), path_1.default.basename(file).split('.')[0] + '.States', entry.StatesPaths[k]);
